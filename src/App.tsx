b6e9f36218545109a8b042e0963ba062ab337ac9
doraemon0807 <98371903+doraemon0807@users.noreply.github.com>
2023-01-01 01:04:52 -0500

    animation final

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,101 @@
-import React from "react";
+import {
+  motion,
+  useMotionValue,
+  useScroll,
+  useTransform,
+  AnimatePresence,
+} from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+
+import styled from "styled-components";
+
+const Wrapper = styled(motion.div)`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(135deg, #e09, #d0e);
+  flex-direction: column;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Box = styled(motion.div)`
+  height: 200px;
+  background-color: white;
+  border-radius: 20px;
+  display: flex;
+  font-size: 28px;
+  border: 1px solid whitesmoke;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const overlayAnimate = {
+  initial: {
+    backgroundColor: "rgba(0, 0, 0, 0)",
+  },
+  animate: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  exit: {
+    backgroundColor: "rgba(0, 0, 0, 0)",
+  },
+};
 
 function App() {
-  return <div></div>;
+  const [id, setId] = useState<string | null>(null);
+  return (
+    <Wrapper>
+      <Grid>
+        {["1", "2", "3", "4"].map((n) => (
+          <Box onClick={() => setId(n)} key={n} layoutId={n}>
+            {n}
+          </Box>
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {id && (
+          <Overlay
+            onClick={() => setId(null)}
+            variants={overlayAnimate}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              default: {
+                duration: 0.2,
+              },
+            }}
+          >
+            <Box layoutId={id} style={{ width: 400, height: 200 }}>
+              {id}
+            </Box>
+          </Overlay>
+        )}
+      </AnimatePresence>
+    </Wrapper>
+  );
 }
 
 export default App;
